a2be8829ed827ab99341a8cdb37710b838d2ed16
Sherlock Holmes <sherlock@Sherlocks-MacBook-Pro.local>
2021-10-03 14:20:04 -0400

    web server

---
+++ README.md
@@ -0,0 +1,7 @@
+# Checklist
+
+- [x] Encode HTML content
+- [x] Comment out print statements
+- [x] Test URL http://127.0.0.1:13331/helloworld.html
+- [x] Include 200 OK status
+- [x] Include 404 Not Found status
--- solution.py
+++ solution.py
@@ -1,34 +1,57 @@
+from socket import *
+import sys
 
-### welcome_assignment_answers
-### Input - All eight questions given in the assignment.
-### Output - The right answer for the specific question.
+def webServer(port=13331):
+    REQUEST_URL = 'http://127.0.0.1:13331/helloworld.html'
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('127.0.0.1', port))
+    serverSocket.listen(5)
 
-def welcome_assignment_answers(question):
-    #The student doesn't have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
-    elif question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
-        answer = "mTLS"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
-    elif question ==  "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
-    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = "42b76fe51778764973077a5a94056724"
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
-    elif question ==  "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 5
-    elif question ==  "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 4
-    return(answer)
-# Complete all the questions.
+    while True:
+        # print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()
+        try:
+            try:
+                message = connectionSocket.recv(1024).decode()
+                filename = message.split()[1]
+                f = open(filename[1:])
+                outputdata = f.read()
 
+                #Send one HTTP header line into socket.
+                OK_CONTENT = b'''HTTP/1.1 200 OK\r\nContent-Type: text/html\n\n'''
+                OK = b'''HTTP/1.1 200 OK\r\n\r\n'''
+                connectionSocket.sendall(OK_CONTENT)
+                connectionSocket.sendall(OK)
 
-if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
+                #Send the content of the requested file to the client
+                for i in range(0, len(outputdata)):
+                    connectionSocket.send(outputdata[i].encode())
+                connectionSocket.send('\r\n'.encode())
+                connectionSocket.close()
+            except IOError:
+                # Send response message for file not found (404)
+                NOT_FOUND_CONTENT = b'''HTTP/1.1 404 Not Found\r\nContent-Type: text/html\n\n'''
+                NOT_FOUND = b'''HTTP/1.1 404 Not Found\r\n'''
+                connectionSocket.sendall(NOT_FOUND_CONTENT)
+                connectionSocket.sendall(NOT_FOUND)
+
+                # Close client socket
+                connectionSocket.close()
+        except (ConnectionResetError, BrokenPipeError):
+            connectionSocket.close()
+        except (KeyboardInterrupt):
+            try:
+                if connectionSocket:
+                    connectionSocket.shutdown()
+                    connectionSocket.close()
+                if serverSocket:
+                    serverSocket.shutdown()
+                    serverSocket.close()
+                sys.exit()
+            except: pass
+            break
+    serverSocket.close()
+    sys.exit()
+
+if __name__ == '__main__':
+    webServer(13331)
