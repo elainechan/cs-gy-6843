3cdacf1bbf7d7aa3c8a543d1eb7346b46bc3842d
Sherlock Holmes <sherlock@sherlocks-mbp.home>
2021-12-07 13:50:57 -0500

    remove print

--- solution.py
+++ solution.py
@@ -43,7 +43,6 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
 
         timeReceived = time.time()
         received_bytes, addr = mySocket.recvfrom(1024)
-        print(len(received_bytes))
 
         # Fetch the ICMP header from the IP packet
         icmp_header = received_bytes[20:28]
@@ -108,14 +107,14 @@ def ping(host, timeout=1):
     print("Pinging " + dest + " using Python:")
     print("")
     # Calculate vars values and return them
-    vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
+    # vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
     # Send ping requests to a server separated by approximately one second
     for i in range(0,4):
         delay = doOnePing(dest, timeout)
         print(delay)
         time.sleep(1)  # one second
-
-    return vars
+    return delay
+    # return vars
 
 if __name__ == '__main__':
     ping("google.co.il")
