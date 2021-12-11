95893164b14e8b8209f40124d248e82298ac8794
Elaine Chan <eightdevelopers@gmail.com>
2021-12-11 13:59:45 -0500

    return when type == 0

--- solution.py
+++ solution.py
@@ -91,6 +91,7 @@ def get_route(hostname):
                 howLongInSelect = (time.time() - startedSelect)
                 if whatReady[0] == []: # Timeout
                     tracelist1.append("* * * Request timed out.")
+                    tracelist2.append(tracelist1)
                     #Fill in start
                     #You should add the list above to your all traces list
                     #Fill in end
@@ -101,6 +102,7 @@ def get_route(hostname):
                 timeLeft = timeLeft - howLongInSelect
                 if timeLeft <= 0:
                     tracelist1.append("* * * Request timed out.")
+                    tracelist2.append(tracelist1)
                     #Fill in start
                     #You should add the list above to your all traces list
                     #Fill in end
@@ -156,7 +158,7 @@ def get_route(hostname):
                     # tracelist2.append(res)
                     #Fill in end
                 elif types == 0:
-                    break
+                    return tracelist2
                 
                     # bytes = struct.calcsize("d")
                     # timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
@@ -165,9 +167,9 @@ def get_route(hostname):
                     # rtt = str(int(round((timeReceived - timeSent) * 1000, 0))) + "ms"
                     # res = [str(ttl), rtt, addr[0], host]
                     
-                    tracelist1.append(addr[0])
-                    tracelist1.append(host)
-                    tracelist2.append(tracelist1)
+                    # tracelist1.append(addr[0])
+                    # tracelist1.append(host)
+                    # tracelist2.append(tracelist1)
                     
                     # tracelist2.append(res)
                     #Fill in end
@@ -187,69 +189,6 @@ def get_route(hostname):
                 mySocket.close()
     return tracelist2
 
-# print(get_route('www.google.com'))
-# get_route('www.google.com')
 if __name__ == "__main__":
     get_route('www.google.com')
-    # print(get_route('www.google.com'))
-
-'''
-All values must be strings
-
-You're also not resolving the hostname of the replying IP; see how it's 
-www.google.com on every hop?  Additionally, you should put a return statement 
-in your elif types == 0, since that's the block where you're getting an ICMP 
-type 0 (an echo reply) which should only come from the destination you're 
-actually tracing to; you want the loop to end at that point.  As is, you're 
-reaching www.google.com at hop 10, and then continuing to send pings to 
-www.google.com for hops 11-29.
-
-In your most recent submission on Gradescope, your get_route definition starts 
-off with initializing both tracelist1 and tracelist2 as empty lists.  You only 
-want to initialize tracelist2 as an empty list here.  tracelist1 should be a 
-list containing:
-The hop number, which is equivalent to the TTL you set on that iteration of the 
-loop
-The round trip time (time between you sending the packet and receiving it).  
-Right now, it looks like you have this set to calculated as 
-timeReceived - timeSent, where 
-timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0] . 
-This looks like it's assuming the ICMP payload was set to be whatever the 
-current time was went the ping was sent, which build_packet doesn't do.  
-To simplify this, I'd instead calculated RTT as timeReceived - startedSelect . 
-startedSelect is set on line 98 as the current time when you send the packet 
-(or almost immediately after--close enough for this assignment); timeReceived 
-is set on line 109 and is the time when you received the reply packet as 
-measured by time.time() immediately after 
-recvPacket, addr = mySocket.recvfrom(1024).
-The source IP address of the reply packet.
-The reverse resolution of that source IP, as given by the 
-gethostbyaddr(addr[0]) function.  You may want to modify this slightly--as-is 
-this actually returns a list of values including the address you looked up and 
-the reverse resolution, but you only want the reverse resolution.  Output below:
->>> from socket import *
->>> gethostbyaddr("8.8.8.8")('dns.google', [], ['8.8.8.8'])
->>> gethostbyaddr("8.8.8.8")[0]
-'dns.google'
-Note that by just pulling out the [0] index of gethostbyaddr, I only get the 
-hostname, not any of the other items.
-If there is no reverse resolution, you can just set 
-srcHostname = "hostname not returnable", which you've done on line 134.
-If you do not get a response from a particular hop, you want to set the content 
-of tracelist1 to reflect that:
-Hop number
-"* Request timed out."
-On each iteration of the for loop you want to re-initialize tracelist1 as an 
-empty list.  Otherwise you're just going to continually append to it and 
-continually append its content to tracelist2.
-Last thing - you've defined tracelist1 as an empty list; within your append 
-statements you're kind of duplicating that effort.
-tracelist1 = []
-...
-tracelist1.append([str(ttl), rtt, str(addr[0]), srcHostname])
-tracelist2.append([tracelist1])
-I think what this would do is append a list containing those four values to 
-tracelist1 , so tracelist1 is a list of a list; tracelist2.append([tracelist1]) 
-then appends a list containing tracelist1, which itself is a list of a list, so 
-tracelist2 becomes a list, of a list, of a list, of a list. (edited) 
-'''
+    print(get_route('www.google.com'))
